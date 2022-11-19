ffe7507fb4883427484a5b48d8c0f4b90c3d2fe7
이동아 <christar99@naver.com>
2022-11-19 23:26:39 +0900

    heroku connect

--- src/model/index.js
+++ src/model/index.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DB
-})
+    database: process.env.MYSQL_DB,
+});
 
 module.exports = connection;
