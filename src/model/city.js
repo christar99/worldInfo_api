5f7755a8d5028d91033b34300db99c3ec6d70bd4
이동아 <christar99@naver.com>
2022-11-19 19:53:31 +0900

    heroku connect test

--- src/model/city.js
+++ src/model/city.js
@@ -1,24 +1,24 @@
-const connection = require('./index');
+const connection = require("./index");
 
 let City = (city) => {
-    this.city.name = city.name
-}
+    this.city.name = city.name;
+};
 
 City.findByIdx = (req, result) => {
-    let query = req.query.name === undefined
-        ? 'SELECT * FROM city'
-        : 'SELECT * FROM city WHERE name = ?';
+    let query =
+        req.query.name === undefined
+            ? "SELECT * FROM city"
+            : "SELECT * FROM city WHERE name = ?";
 
     connection.query(query, [req.query.idx], (err, res) => {
         console.log(res);
-        if(err) {
-            console.log("error: " + err);
+        if (err) {
+            console.log("error: ", err);
             result(null, err);
-        }
-        else {
+        } else {
             result(null, res);
         }
-    })
-}
+    });
+};
 
 module.exports = City;
