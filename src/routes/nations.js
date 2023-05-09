22b158802794902dfb7783a7060027d6d05e9cc5
이동아 <christar99@naver.com>
2023-05-09 11:32:54 +0000

    prettier setting

--- src/routes/nations.js
+++ src/routes/nations.js
@@ -1,7 +1,7 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const controller = require('../controller/nations.controller');
+const controller = require("../controller/nations.controller");
 
-router.get('/', controller.findByName);
+router.get("/", controller.findByName);
 
 module.exports = router;
